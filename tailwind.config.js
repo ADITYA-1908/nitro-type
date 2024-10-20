42dce41f89d23759755fe5170348d9f5335dd8f7
ADITYA BASANTIA <adityabasantia729@gmail.com>
2024-10-20 19:40:08 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    rotate: {
+      "-180": "-180deg",
+      "-90": "-90deg",
+      "-45": "-45deg",
+      0: "0",
+      45: "45deg",
+      90: "90deg",
+      135: "135deg",
+      180: "180deg",
+      270: "270deg",
+    },
     extend: {
+      fontFamily: {
+        Header: ["Lato", "sans-serif"],
+        Text2: ["Lato", "sans-serif"],
+        About :["Playfair Display", "serif"]
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        AAprimary: "#0b192f",
+        AAsecondary: "#64ffda",
+        AAError:"#ff6489",
+        AAtertiary: "#112340",
+        ResumeButtonHover: "#153040",
+        MobileNavBarColor: "#112340",
+        StartupBackground: "#020c1b",
       },
     },
   },
   plugins: [],
-};
+}
